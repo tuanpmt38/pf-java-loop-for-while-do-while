0c3f5cd2d1614f66f9afcef768f9c71d90e47d88
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-03-18 22:51:21 +0700

    vi du for while, do while

---
+++ LoopFor.java
@@ -0,0 +1,40 @@
+public class LoopFor {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+        // vong lap while
+//            int i = 1;
+//            while (i <= 10) {
+//                System.out.println(i);
+//                i++;
+//            }
+
+        //vong lap do-while
+//            int a = 1, sum = 0;
+//            do {
+//                sum += a;
+//                a++;
+//            } while (a <= 5);
+//            System.out.println("Sum of 1 to 5  is " + sum);
+
+        //break trong java
+//            for (int i = 1; i <= 10; i++) {
+//                if (i == 5) {
+//                    break;
+//                }
+//                System.out.println(i);
+//            }
+
+        //continue
+//            for (int i = 1; i <= 10; i++) {
+//                if (i == 5) {
+//                    continue;
+//                }
+//                 Khi i == 5 thì không in i = 5 ra màn hình
+//                System.out.println(i);
+//            }
+
+
+    }
+}
